24e914c687d8c67d1a023066ce3833bd3b17b60d
DanielBuenoNavarroo <danbue419@alu.centroafuera.es>
2023-11-03 14:19:41 +0100

    main y primer layout

--- app/src/main/java/com/example/ejercicio6/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio6/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ejercicio6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLayout1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLayout1 = findViewById(R.id.LL1)
+        btnLayout1.setOnClickListener {
+            val intent = Intent(this@MainActivity, LinearLayout1::class.java)
+            startActivity(intent)
+        }
     }
 }
